9efd93e18c7143e502d22fee23423135b197eac1
André Gumieri <andregumieri@gmail.com>
2020-02-15 17:31:53 -0300

    Init

---
+++ monitor.py
@@ -0,0 +1,61 @@
+# @see https://www.embarcados.com.br/raspberry-pi-gpio-modo-input-python/
+import RPi.GPIO as gpio
+import os
+import time
+import requests
+import config
+
+gpio.setmode(gpio.BCM)
+
+gpio.setup(23, gpio.IN, pull_up_down = gpio.PUD_DOWN)
+gpio.setup(24, gpio.IN, pull_up_down = gpio.PUD_DOWN)
+gpio.setup(27, gpio.OUT)
+
+
+
+
+def doorbell(which, holding=False, previous_alert=0):
+    labels = {'front': 'front door', 'back': 'back door'}
+
+    if(holding == True or time.time() - previous_alert < 15):
+        return previous_alert
+
+    # alerta
+    return alert(labels[which])
+
+
+def alert(label):
+    print 'Alerting ' + label
+    payload = {'value1': label}
+    r = requests.post("https://maker.ifttt.com/trigger/doorbell/with/key/" + config.IFTTT_TOKEN, params=payload)
+    return time.time()
+
+
+def alert_init():
+    requests.post("https://maker.ifttt.com/trigger/doorbell_init/with/key/" + config.IFTTT_TOKEN)
+
+
+
+holding = {'front': False, 'back': False}
+previous_alert = {'front': 0, 'back': 0}
+ios = {23: 'back', 24: 'front'}
+
+
+alert_init()
+
+while True:
+    pressing = False
+
+    for io in ios:
+        slug = ios[io]
+
+        if(gpio.input(io) == 1):
+            previous_alert[slug] = doorbell(slug, holding[slug], previous_alert[slug])
+            holding[slug] = True
+            pressing = True
+        else:
+            holding[slug] = False
+
+
+gpio.cleanup()
+exit()
